f3b777a556a64f0119c007fcceb31845de8e128d
Kadidja Mohamed Daoud <katymiarosa2004@gmail.com>
2024-08-16 14:38:19 +0200

    Update ficheJson.py

--- ficheJson.py
+++ ficheJson.py
@@ -1,6 +1,6 @@
 import json
 # Ouvrir le fichier JSON en mode lecture
-with open('/content/Untitled Folder/sampleJson (1).json', 'r') as fichier:
+with open('sampleJson.json', 'r') as fichier:
     # Lire le contenu du fichier et le charge en tant qu'objet Python
     r_fichier = json.load(fichier)
 # Afficher le contenu du fichier JSON
